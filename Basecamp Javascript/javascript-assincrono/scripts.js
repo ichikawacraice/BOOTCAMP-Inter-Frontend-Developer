803257dad3d75381020fa7bb07d62ffc748eb39b
ichikawacraice <g.craice@gmail.com>
2022-01-19 04:07:11 -0300

    javascript assincrono

---
+++ Basecamp Javascript/javascript-assincrono/scripts.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const btnCat = document.querySelector('#change-cat');
+const imgCat = document.querySelector('#cat');
+
+const getCats = async () => {
+    const data = await fetch(BASE_URL)
+    .then((res) => res.json())
+    .catch((e) => console.log(e))
+
+    return data.webpurl;
+};
+
+const loadImg = async () => {
+    imgCat.src = await getCats();
+};
+
+btnCat.addEventListener('click', loadImg);
+
+loadImg();
